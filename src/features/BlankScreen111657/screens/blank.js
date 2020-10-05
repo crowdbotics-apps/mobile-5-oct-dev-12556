d15a3f5dcb64156b348f2133b6e97116bc67ca2c
Crowdbotics <team@crowdbotics.com>
2020-10-05 05:57:14 +0000

    Update components

--- src/features/BlankScreen111657/screens/blank.js
+++ src/features/BlankScreen111657/screens/blank.js
@@ -19,7 +19,7 @@ export class __Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = { DateTimePicker_3: new Date("10/05/2020"), Switch_5: true }
+  state = { DateTimePicker_3: new Date("10/05/2020"), TextInput_4: "" }
   render = () => (
     <View style={styles.View_1}>
       <Text style={styles.Text_2}>Sample text content</Text>
@@ -31,20 +31,13 @@ export class __Blank extends React.Component {
           this.setState({ DateTimePicker_3: selectedDate })
         }
       />
-      <Button
-        title="Press me!"
-        color="#3366FF"
-        style={styles.Button_4}
-        onPress={() => alert("Pressed!")}
-      />
-      <Switch
-        activeColor="#409EFF"
-        inactiveColor="#C0CCDA"
-        disabled={false}
-        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
-        style={styles.Switch_5}
-        value={this.state.Switch_5}
-        onValueChange={nextChecked => this.setState({ Switch_5: nextChecked })}
+      <TextInput
+        placeholder="Sample text input placeholder"
+        editable={true}
+        multiline={true}
+        style={styles.TextInput_4}
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
       />
       <Slider
         value={50}
@@ -55,7 +48,7 @@ export class __Blank extends React.Component {
         maximumTrackTintColor="#E4E7ED"
         minimumTrackTintColor="#3366FF"
         thumbTintColor="#3366FF"
-        style={styles.Slider_6}
+        style={styles.Slider_5}
       />
     </View>
   )
